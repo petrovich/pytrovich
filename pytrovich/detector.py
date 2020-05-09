00450f789ca23e2b800cbd96b151f6dd2cd0e86a
alexeyev <anton.m.alexeyev@gmail.com>
2020-05-09 15:29:48 +0300

    [ENH] Gender prediction algorithm -- code checking suffixes

--- pytrovich/detector.py
+++ pytrovich/detector.py
@@ -22,8 +22,9 @@ class PetrovichGenderPredictor(object):
             self._root_rules_bean = Root.parse(rules_data.gender()["gender"])
 
     @staticmethod
-    def _check_against_exceptions(name: Name, str_name: str):
+    def _check_against_exceptions(name: Name, str_name: str) -> set:
         results = []
+
         if name.exceptions and name.exceptions.male and str_name in name.exceptions.male:
             results.append(Gender.MALE)
         if name.exceptions and name.exceptions.female and str_name in name.exceptions.female:
@@ -32,18 +33,51 @@ class PetrovichGenderPredictor(object):
             results.append(Gender.ANDROGYNOUS)
         return set(results)
 
+    @staticmethod
+    def _check_again_suffixes(name: Name, str_name: str) -> set:
+
+        results = []
+
+        if name.suffixes and name.suffixes.male:
+            for possible_suffix in name.suffixes.male:
+                if str_name.endswith(possible_suffix):
+                    results.append(Gender.MALE)
+                    break
+
+        if name.suffixes and name.suffixes.female:
+            for possible_suffix in name.suffixes.female:
+                if str_name.endswith(possible_suffix):
+                    results.append(Gender.FEMALE)
+                    break
+
+        if name.suffixes and name.suffixes.andro:
+            for possible_suffix in name.suffixes.andro:
+                if str_name.endswith(possible_suffix):
+                    results.append(Gender.ANDROGYNOUS)
+                    break
+
+        return set(results)
+
     def detect(self, firstname=None, lastname=None, middlename=None):
+
         assert not (firstname is None and lastname is None and middlename is None), \
             "At least one part of the name should be given."
 
-        results = {}
+        results = set([])
 
         if middlename:
-            results.update(PetrovichGenderPredictor._check_against_exceptions(self._root_rules_bean.middlename, middlename))
+            results.update(
+                PetrovichGenderPredictor._check_against_exceptions(self._root_rules_bean.middlename, middlename))
+            results.update(
+                PetrovichGenderPredictor._check_again_suffixes(self._root_rules_bean.middlename, middlename))
         if firstname:
-            results.update(PetrovichGenderPredictor._check_against_exceptions(self._root_rules_bean.firstname, firstname))
+            results.update(
+                PetrovichGenderPredictor._check_against_exceptions(self._root_rules_bean.firstname, firstname))
+            results.update(
+                PetrovichGenderPredictor._check_again_suffixes(self._root_rules_bean.firstname, firstname))
         if lastname:
             results.update(PetrovichGenderPredictor._check_against_exceptions(self._root_rules_bean.lastname, lastname))
+            results.update(PetrovichGenderPredictor._check_again_suffixes(self._root_rules_bean.lastname, lastname))
         # todo: rule check in progress
 
         return results
