386d0848bc9fcb363f355ecf253f9b600105f0e3
Anton Alekseev <anton.m.alexeyev@gmail.com>
2026-05-04 23:52:12 +0300

    [ENH] new release

--- pytrovich/meta.py
+++ pytrovich/meta.py
@@ -1,5 +1,5 @@
 package = "pytrovich"
-version = "0.0.6"
+version = "0.0.7"
 authors = ["Anton Alekseev"]
 authors_string = ", ".join(authors)
 emails = ["anton.m.alexeyev+petrovich@gmail.com"]
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", encoding="utf-8") as fh:
 setup(
     name="pytrovich",
     packages=setuptools.find_packages(exclude=("tests", "tests.*")),
-    version="0.0.6",
+    version="0.0.7",
     description="pytrovich: a Python port of an inflector for Russian anthroponyms developed by petrovich team: "
     "https://github.com/petrovich",
     long_description=long_description,
