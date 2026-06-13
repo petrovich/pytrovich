29d7f7e816bb765d7bfcbca29b7365601bf66c5f
Anton Alekseev <anton.m.alexeyev@gmail.com>
2026-06-13 23:23:16 +0300

    [ENH] Ruby version parity tested, hyphenated surnames should work now

--- tests/test_ruby_parity.py
+++ tests/test_ruby_parity.py
@@ -132,7 +132,10 @@ class TestGenderDetectionRubySemantics:
         assert detector.detect(firstname="Саша", lastname="Иванов") == Gender.MALE
 
     def test_confident_patronymic_wins(self, detector):
-        assert detector.detect(firstname="Саша", lastname="Андрейчук", middlename="Олегович") == Gender.MALE
+        assert (
+            detector.detect(firstname="Саша", lastname="Андрейчук", middlename="Олегович")
+            == Gender.MALE
+        )
 
     def test_hyphenated_part_resolves_on_last_component(self, detector):
         assert detector.detect(middlename="Мухаммад-кызы") == Gender.FEMALE
